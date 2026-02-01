b7c8fee06d5d897d4358a7cb7f4f487fef06d26c
VarunKumar988 <cvarunkumar455@gmail.com>
2026-02-01 17:25:15 +0530

    changes

--- src/features/jobs/components/CountrySelector.tsx
+++ src/features/jobs/components/CountrySelector.tsx
@@ -2,11 +2,7 @@
 
 import { COUNTRIES, Country } from "@/constants/countries";
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
+    Select
 } from "@/components/ui/Select";
 import { Globe } from "lucide-react";
 
@@ -19,21 +15,12 @@ export function CountrySelector({ value, onChange }: CountrySelectorProps) {
     const selected = COUNTRIES.find(c => c.code === value) || COUNTRIES[0];
 
     return (
-        <Select value={value} onValueChange={onChange}>
-            <SelectTrigger className="w-[180px] h-12 bg-white border-gray-300 focus:ring-blue-500/20 gap-2">
-                <Globe className="h-4 w-4 text-gray-500" />
-                <SelectValue>{selected.name}</SelectValue>
-            </SelectTrigger>
-            <SelectContent>
-                {COUNTRIES.map((country) => (
-                    <SelectItem key={country.code} value={country.code}>
-                        <span className="flex items-center gap-2">
-                            <span>{country.name}</span>
-                            <span className="text-gray-400 text-xs">({country.currency})</span>
-                        </span>
-                    </SelectItem>
-                ))}
-            </SelectContent>
+        <Select value={value} onChange={(e) => onChange(e.target.value)}>
+            {COUNTRIES.map((country) => (
+                <option key={country.code} value={country.code}>
+                    {country.name} ({country.currency})
+                </option>
+            ))}
         </Select>
     );
 }
